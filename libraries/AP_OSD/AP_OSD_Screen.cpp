14f6f702d6eae0a7d21279a267669e3da56e7daf
StonedDawg <st.kenneth93@gmail.com>
2021-05-28 15:43:06 +0700

    add lq

--- libraries/AP_OSD/AP_OSD_Screen.cpp
+++ libraries/AP_OSD/AP_OSD_Screen.cpp
@@ -1130,7 +1130,9 @@ void AP_OSD_Screen::draw_rssi(uint8_t x, uint8_t y)
     AP_RSSI *ap_rssi = AP_RSSI::get_singleton();
     if (ap_rssi) {
         const uint8_t rssiv = ap_rssi->read_receiver_rssi() * 99;
+        const uint8_t qualityv = ap_rssi->read_receiver_quality() * 99;
         backend->write(x, y, rssiv < osd->warn_rssi, "%c%2d", SYM_RSSI, rssiv);
+        backend->write(x, y+10, qualityv < osd->warn_rssi, "%c%2d", SYM_RSSI, rssiv);
     }
 }
 
--- libraries/AP_RCProtocol/AP_RCProtocol.cpp
+++ libraries/AP_RCProtocol/AP_RCProtocol.cpp
@@ -352,7 +352,13 @@ int16_t AP_RCProtocol::get_RSSI(void) const
     }
     return -1;
 }
-
+int16_t AP_RCProtocol::get_quality(void) const
+{
+    if (_detected_protocol != AP_RCProtocol::NONE) {
+        return backend[_detected_protocol]->get_quality();
+    }
+    return -1;
+}
 /*
   ask for bind start on supported receivers (eg spektrum satellite)
  */
