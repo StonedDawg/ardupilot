55a8f4f118c4078a346c04f5d7e830248550756f
StonedDawg <st.kenneth93@gmail.com>
2021-07-07 11:50:49 +0700

    flip rssi osd warning

--- libraries/AP_OSD/AP_OSD_Screen.cpp
+++ libraries/AP_OSD/AP_OSD_Screen.cpp
@@ -1132,7 +1132,7 @@ void AP_OSD_Screen::draw_rssi(uint8_t x, uint8_t y)
         const uint8_t rssiv = ap_rssi->read_receiver_rssi();
         const uint8_t qualityv = ap_rssi->read_receiver_quality();
         const uint8_t rfmode2v = ap_rssi->read_receiver_rfmode2();
-        backend->write(x, y, rssiv < osd->warn_rssi, "%c%d.%d.-%d", SYM_RSSI, rfmode2v,qualityv,rssiv);
+        backend->write(x, y, rssiv > osd->warn_rssi, "%c%d.%d.-%d", SYM_RSSI, rfmode2v,qualityv,rssiv);
     }
 }
 
